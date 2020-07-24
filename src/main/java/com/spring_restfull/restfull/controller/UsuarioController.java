6162e70dc1573c56dbf20e95f603ed0f41b59530
Vanderlei K Silva <vanderleiksilva@gmail.com>
2020-07-24 20:59:57 -0300

    added index.html and crossorigin

--- src/main/java/com/spring_restfull/restfull/controller/UsuarioController.java
+++ src/main/java/com/spring_restfull/restfull/controller/UsuarioController.java
@@ -9,6 +9,7 @@ import com.spring_restfull.restfull.repositories.UsusarioRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/usuario")
 public class UsuarioController {
